3fe7884cd3ef964d9e0483f9071d0d8b75652612
jayanth <jayanthshetty660@gmail.com>
2025-10-22 22:19:48 +0530

    added client side routing for img slideshow

--- app/page.js
+++ app/page.js
@@ -1,14 +1,51 @@
 import Link from "next/link";
-
+import classes from './page.module.css';
+import ImageSlideshow from "@/components/image/image-slideshow";
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href='/meals'>Meals</Link></p>
-        <Link href='/meals/share'>Share Meal</Link>
-      <p><Link href='/community'>Community</Link></p>
+    <>
+    <header className={classes.header}>
+      <div className={classes.slideshow}>
+        <ImageSlideshow/>
+      </div>
+      <div>
+        <div className={classes.hero}>
+          <h1>Next level food for next level Foodies</h1>
+          <p>Paste and share food all over the world</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href='/community'>Join the Comunity</Link>
+          <Link href='/meals'>Explore meals</Link>
+        </div>
+      </div>
+    </header>
+    <main >
+      <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
     </main>
+    </>
   );
 }
